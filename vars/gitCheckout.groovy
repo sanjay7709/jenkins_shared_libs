bf72d6723b3f99affacb698bd6e75a0c85eec3f3
sanjay7709 <sanjaysundarmurthy@gmail.com>
2023-09-29 14:36:44 +0530

    jenkins file

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -3,7 +3,7 @@ def call(Map stageparams){
     checkout([
         $class: 'GitSCM'
         branches: [[name: 'stageParams.branch ']],
-        userRemoteConfigs: [[ url: stageParams.url ]]
+        userRemoteConfigs: [[ url: 'stageParams.url' ]]
     ])
 
 }

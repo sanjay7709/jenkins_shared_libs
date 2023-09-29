f18daaf7934dac222e5b2e4c0a543e9db494ffbe
sanjay7709 <sanjaysundarmurthy@gmail.com>
2023-09-29 15:55:03 +0530

    jenkins file

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,6 +2,6 @@ def call(String project, String hubuser, String Imagetag){
     sh '''
         docker image build ${hubuser}/${project} .
         docker image tag ${hubuser}/${project} ${hubuser}/${project}:${Imagetag}
-        docker image tag ${hubuser}/${project} ${hubuser}/${project}:v1
+        docker image tag ${hubuser}/${project} ${hubuser}/${project}:latest
     '''
 }

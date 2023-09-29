daf6fc53627da17afbae1f5e6eddcac4fc49d7a2
sanjay7709 <sanjaysundarmurthy@gmail.com>
2023-09-29 22:49:55 +0530

    jenkins file

---
+++ vars/kubernetesDeploy.groovy
@@ -0,0 +1,6 @@
+def call(String project, String hubuser, String ACR_LOGINSERVER, String ImageTag){
+    sh """
+        kubectl set image deployment/${ACR_LOGINSERVER}/${project}:${ImageTag} --kubeconfig /var/lib/jenkins/config
+        kubectl apply -f deployment.yml --kubeconfig /var/lib/jenkins/config
+    """
+}

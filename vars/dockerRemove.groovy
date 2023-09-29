ff53cc37760fa636d861819adcca56a2d476419b
sanjay7709 <sanjaysundarmurthy@gmail.com>
2023-09-29 19:27:21 +0530

    jenkins file

--- vars/dockerRemove.groovy
+++ vars/dockerRemove.groovy
@@ -1,6 +1,6 @@
 def call(String project, String hubuser, String ImageTag){
     sh """
-        docker rmi ${hubuser}/${project} ${hubuser}/${project}:${ImageTag}
-        docker rmi ${hubuser}/${project} ${hubuser}/${project}:latest
+        docker rmi ${hubuser}/${project}:${ImageTag}
+        docker rmi ${hubuser}/${project}:latest
     """
 }
